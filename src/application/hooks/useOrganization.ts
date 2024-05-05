b3a9e2f41ef21a046f4270947c9e759c353a181d
unknown <kadiksalah03@gmail.com>
2024-05-05 16:25:58 +0100

    fixed some issues

--- src/application/hooks/useOrganization.ts
+++ src/application/hooks/useOrganization.ts
@@ -11,8 +11,9 @@ export default function useOrganization(): useOrganizationReturn {
     const [organization, setOrganization] = useState<OrganizationEntity>(initOrganization);
     const [events, setEvents] = useState<EventEntity[]>([]);
     const [challenges, setChallenges] = useState<ChallengeEntity[]>([]);
-    const [isChallengesLoading, setIsChallengesLoading] = useState(true);    
-    const [isLoading, setIsLoading] = useState(false);
+    const [isChallengesLoading, setIsChallengesLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+
     const getOrganizationById = async (organizationId: number): Promise<void> => {
         const response = await organizationsService.getOrganizationById(organizationId);
         setIsLoading(false);
@@ -43,12 +44,10 @@ export default function useOrganization(): useOrganizationReturn {
         }
     }
 
-   
-
     return {
         organization, getOrganizationById,
         events, getOrganizationEvents,
         challenges, getOrganizationChallenges,
-        isLoading, isChallengesLoading,
+        isLoading, isChallengesLoading
     };
 }
