2ce9df6f46e2c13a13f0d9447c586ffb3fe3ab38
unknown <kadiksalah03@gmail.com>
2024-05-10 11:45:09 +0100

    fixed conflict

--- src/presentation/components/dashboard/OrganizationJobPosts.tsx
+++ src/presentation/components/dashboard/OrganizationJobPosts.tsx
@@ -1,22 +1,16 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import OrganizationDashboardContext from "../../../application/contexts/OrganizationDashboardContext";
 import { useParams } from "react-router-dom";
 import LoadingIndicator from "../LoadingIndicator";
 import usePopup from "../../../application/hooks/usePopup";
 import MyJobPostCard from "../MyJobPostCard";
-import DashboardModel from "./DashboardModel";
-import JobApplications from "./JobApplications";
-import useDashboardModel from "../../../application/hooks/useDashboardModel";
 
 export default function OrganizationJobPosts() {
     const { id } = useParams();
     const { jobPosts, isJobPostsLoading, getOrganizationJobPosts } = useContext(OrganizationDashboardContext);
-    const popupContentOptions: PopupContent = { title: 'jobPost', method: 'create' }
-    const { onopen, children } = usePopup(popupContentOptions);
-
-    const useDashboardModelValue = useDashboardModel();
-    const [selectedJobPost, setSelectedJobPost] = useState<JobPostEntity>({ title: '', contractType: 'full-time', description: '', location: '', organization_id: 0, role: '', type: 'remote' });
-
+    const popupContentOptions: PopupContent = {title: 'jobPost',method:'create'}
+    const {  onopen, children } = usePopup(popupContentOptions);
+  
     useEffect(() => {
         if (id && isJobPostsLoading) {
             getOrganizationJobPosts(parseInt(id));
@@ -34,8 +28,8 @@ export default function OrganizationJobPosts() {
                 </div>
                 <button
                     onClick={onopen}
-                    className={`font-medium px-3 py-1 rounded-lg mt-5 hover:opacity-90 active:scale-105 transition-all duration-300 bg-primary-yellow text-nowrap`}
-                >
+                    className={`font-meduim px-3 py-1 rounded-lg mt-5 hover:opacity-90 active:scale-105 transition-all duration-300 bg-primary-yellow text-nowrap`}
+                    >
                     Create Job Post
                 </button>
             </div>
@@ -50,14 +44,9 @@ export default function OrganizationJobPosts() {
                     }
                     {
                         jobPosts.map((jobPost, index) => {
-                            const viewJobApplications = () => {
-                                setSelectedJobPost(jobPost);
-                                useDashboardModelValue.open();
-                            }
-
                             return (
                                 <div key={index} className="w-1/4 pr-4 mb-4">
-                                    <MyJobPostCard jobPost={jobPost} viewJobApplications={viewJobApplications} />
+                                    <MyJobPostCard jobPost={jobPost}/>
                                 </div>
                             )
                         })
@@ -65,9 +54,6 @@ export default function OrganizationJobPosts() {
                 </div>
             </div>
             {children}
-            <DashboardModel useDashboardModel={useDashboardModelValue} >
-                <JobApplications jobPost={selectedJobPost} />
-            </DashboardModel>
         </div>
     )
 }
--- src/presentation/pages/SingleJobPost.tsx
+++ src/presentation/pages/SingleJobPost.tsx
@@ -1,62 +1,133 @@
 import { Icon } from '@iconify/react'
-import GradientColor from '../../application/data/GradientColor.ts'
-import JobPostCard from '../components/JobPostCard'
 import { NavLink, useParams } from 'react-router-dom'
 import useJobPost from '../../application/hooks/useJobPost.ts'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import useJobPosts from '../../application/hooks/useJobPosts.ts'
+import { organizationProfilePicture, styles } from '../../application/consts.ts'
+import JobPostCardForSearchResult from '../components/JobPostCardForSearchResult.tsx'
 
 function SingleJobPost() {
-    const {styles} = GradientColor()
-    const {getJobPostById,applyJobPost, jobPost} = useJobPost()
-    const {id} = useParams()
-    const {jobPosts} = useJobPosts()
-    useEffect(() => {
-      if(id) {
-        getJobPostById(parseInt(id))
-      }
-    }, [])
+  const { id } = useParams();
+  const { jobPosts } = useJobPosts();
+  const { getJobPostById, jobPost, jobApplication, setJobApplication, resumeFile, setResumeFile, applyJobPost } = useJobPost();
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (id) {
+      getJobPostById(parseInt(id));
+    }
+  }, []);
+
+  const selectResumeFile = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  }
+
+  const onSelectFile = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    ev.preventDefault();
+    if (ev.target.files && ev.target.files?.length > 0) {
+      setResumeFile(ev.target.files[0]);
+    }
+  }
+
+  const removeFile = () => {
+    setResumeFile(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+  }
 
   return (
-    <div className='p-16 mt-8 ml-12'>
-            {jobPost && (
-        <div className='flex my-10'>
-            <div className='w-1/2 flex flex-col gap-5 items-start'>
-                <img src={jobPost.organization?.profile_image_url} alt="Company Image" className='w-20 h-20'/>
-                <div className='flex flex-col gap-1 items-start'>
-                  <h1 className='font-semibold text-3xl'>{jobPost.title}</h1>
-                  <p className='font-light text-lg opacity-75'>{jobPost.role}</p>
-                </div>
-                <NavLink to='/single-organization/2' className='flex items-center gap-3 hover:text-primary-blue transition-colors duration-150'>
-                    <Icon icon="mdi:company" width="16" height="16" />
-                    <span>{jobPost.organization?.creator?.first_name as any} {jobPost.organization?.creator?.last_name as any}</span>
-                </NavLink>
-                <div className='flex items-center gap-3 '>
-                    <Icon icon="carbon:location-company-filled" width="16" height="16" />
-                    <span>{jobPost.location}</span>
-                </div>
-                <button onClick={() => {applyJobPost(parseInt(id as string))}} className={`px-2 py-1 text-lg rounded-lg  ${styles.active} ${styles.from} ${styles.from_prc} ${styles.to} ${styles.to_prc}`}>Apply</button>
+    <div className='w-full flex flex-col items-center'>
+      <div className='w-full flex my-10'>
+        <div className='w-1/2 flex flex-col mb-8 pr-8'>
+          <div className='w-full flex flex-col items-start'>
+            <img src={jobPost.organization?.profile_image_url || organizationProfilePicture} alt="Company Image" className='w-20 rounded-lg mb-2' />
+            <div className='flex flex-col items-start mb-4'>
+              <h1 className='font-semibold text-xl'>{jobPost.title}</h1>
+              <p className='font-light text-gray-400'>{jobPost.role}</p>
             </div>
-            <div className='py-8 px-5 border rounded-xl border-white w-1/2 h-96 scrollbar-none  overflow-scroll '>
-              {jobPost.description}
+            <NavLink to='/single-organization/2' className='flex items-center hover:text-primary-blue transition-colors duration-150'>
+              <Icon icon="mdi:company" width="16" height="16" />
+              <span className='ml-2'>{jobPost.organization?.name}</span>
+            </NavLink>
+            <div className='flex items-center'>
+              <Icon icon="mdi:location" width="16" height="16" />
+              <span className='ml-2'>{jobPost.location}</span>
             </div>
+            <div className="w-full flex items-center mb-4">
+              <div className="flex items-center text-gray-50">
+                <Icon icon="material-symbols:work" />
+                <div className="text-sm font-semibold ml-2">{jobPost.contractType}</div>
+                <div className="text-sm font-semibold ml-1">{`(${jobPost.type})`}</div>
+              </div>
+            </div>
+          </div>
+          {
+            !jobPost.didApply ? (
+              <div className='w-full flex flex-col'>
+                <div className='text-gray-50 font-medium mb-2'>Cover Message:</div>
+                <textarea
+                  onChange={(ev: any) => { setJobApplication(state => ({ ...state, cover_message: ev.target.value })); }}
+                  value={jobApplication.cover_message}
+                  placeholder='Type your cover message...'
+                  className='w-2/3 bg-gray-900 rounded-lg px-4 py-2 h-40 outline-none focus:bg-gray-800 mb-4'
+                />
+                <div className='text-gray-50 font-medium mb-2'>Upload Your Resume:</div>
+                {
+                  resumeFile ? (
+                    <div className='w-full flex items-center justify-between'>
+                      <div className='w-fit text-yellow-500 flex items-center hover:text-yellow-600 cursor-pointer'><Icon icon="bx:file" className='mr-2' />{resumeFile.name}</div>
+                      <div onClick={removeFile} className='text-red-600 text-xs cursor-pointer'><Icon icon="fa:remove" /></div>
+                    </div>
+                  ) : (
+                    <div onClick={selectResumeFile} className='w-fit border-1.5 border-yellow-500 text-yellow-500 px-8 py-1 rounded-lg flex items-center hover:text-yellow-600 cursor-pointer'><Icon icon="bx:file" className='mr-2' />Click to upload</div>
+                  )
+                }
+                <input onChange={onSelectFile} ref={fileInputRef} type='file' accept='.pdf, .docs' hidden />
+                <div className='w-full flex flex-col mt-8'>
+                  <div onClick={() => { applyJobPost(jobPost.id!); }} className={`w-fit ${styles.active} ${styles.from} ${styles.from_prc} ${styles.to} ${styles.to_prc} px-8 py-1 rounded-lg mt-4 cursor-pointer hover:bg-green-600 duration-300 active:scale-110 select-none`}>Submit Application</div>
+                </div>
+              </div>
+            ) : (
+              <div className='w-full flex flex-col'>
+                <div className='w-fit bg-green-500 px-8 py-1 rounded-lg flex items-center select-none'><Icon icon="bx:file" className='mr-2' /> Applied</div>
+              </div>
+            )
+          }
         </div>
-        )}
-        <hr className='my-10'/>
-        <div>
-            <h1 className='font-semibold'>Similar Jobs</h1>
-            <div className="flex my-10 gap-16 min-w-full">
-            {jobPost && jobPosts && jobPosts.map((job, index) => (
-              <div key={index} >
-                {jobPost.title == job.title && (<JobPostCard cardStyle={'Small'}  jobData={{id:job.id as number,"logo":'https://w7.pngwing.com/pngs/606/802/png-transparent-meta-meta-logo-facebook-fb-logo-meta-icon-meta-symbol-facebook-logo-thumbnail.png', "company":'Meta', "skill":"Fluter", 'location':'medea', 'tag':"App development", date_posted:"2 days ago", tag_color:'green-700'}}/>)
+        <div className='w-1/2 h-fit max-h-screen overflow-auto py-8 px-5 border rounded-xl border-white whitespace-pre-line'>
+          {jobPost.description}
+        </div>
+      </div>
+      {
+        jobPosts.length > 0 && (
+          <>
+            <hr className='my-10' />
+            <div className='w-full'>
+              <h1 className='w-full text-xl font-semibold mb-8'>Similar Jobs:</h1>
+              <div className="w-full flex flex-wrap">
+                {
+                  jobPosts.map((job, index) => {
+                    if (job.id == jobPost.id) {
+                      return '';
+                    }
+
+                    return (
+                      <div key={index} className='w-1/4 pr-2 pb-2'>
+                        <JobPostCardForSearchResult jobPost={job} />
+                      </div>
+                    )
+                  })
                 }
               </div>
-            ))}
-          </div>
-        </div>
-        
+            </div>
+          </>
+        )
+      }
     </div>
   )
 }
-
 export default SingleJobPost
