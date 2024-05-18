a41049d9f50300147f1908323e3b362f1f1b2776
unknown <kadiksalah03@gmail.com>
2024-05-18 17:55:03 +0100

    Implement add members modal ui

--- src/presentation/components/dashboard/OrganizationMembers.tsx
+++ src/presentation/components/dashboard/OrganizationMembers.tsx
@@ -1,21 +1,22 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import OrganizationDashboardContext from "../../../application/contexts/OrganizationDashboardContext";
 import LoadingIndicator from "../LoadingIndicator";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import MemberCard from "./MemberCard";
+import DashboardModel from "./DashboardModel";
+import useDashboardModel from "../../../application/hooks/useDashboardModel";
 
 
 export default function OrganizationMembers() {
     const { id } = useParams();
     const { members, getOrganizationMembers, isMembersLoading } = useContext(OrganizationDashboardContext);
+    const useDashboardModelValue = useDashboardModel();
     useEffect(() => {
         if (id && isMembersLoading) {
             getOrganizationMembers(parseInt(id));
         }
     }, [id]);
-
-   
 
     return (
         <div className="w-full h-full flex flex-col bg-gray-950 shadow-2xl shadow-gray-900 rounded-xl px-8 py-5">
@@ -25,10 +26,11 @@ export default function OrganizationMembers() {
                     <div className="text-xs text-gray-300">{"Manage your organization members and their account permissions here"}</div>
                 </div>
                 <button
-                    className={`w-fit font-meduim px-8 py-1 rounded-lg hover:opacity-90 active:scale-105 transition-all duration-300 bg-primary-yellow text-nowrap flex items-center mb-8`}
+                    className={`w-fit font-medium px-8 py-1 rounded-lg hover:opacity-90 active:scale-105 transition-all duration-300 bg-primary-yellow text-nowrap flex items-center mb-8`}
+                    onClick={useDashboardModelValue.open}
                 >
                     <Icon icon="tdesign:member" width="18" height="18" />
-                <div className="ml-2 font-medium">Add Member</div>
+                    <div className="ml-2 font-medium">Add Member</div>
                 </button>
             </div>
             <div className="w-full flex-grow flex flex-col">
@@ -65,6 +67,60 @@ export default function OrganizationMembers() {
                     </div>
                 )
             }
+            <DashboardModel useDashboardModel={useDashboardModelValue} >
+                <UsersList />
+            </DashboardModel>
         </div>
     )
+}
+
+function UsersList() {
+    const [query, setQuery] = useState<string>('');
+    const [selectedUser, setSelectedUser] = useState<number | null>(null);
+    const users = [
+        { id: 1, name: "User 1", email: "kadiksalah03@gmail.com" },
+        { id: 2, name: "User 2", email: "user2@example.com" },
+        { id: 3, name: "User 3", email: "user3@example.com" },
+        { id: 4, name: "User 4", email: "user4@example.com" },
+        { id: 5, name: "User 5", email: "user5@example.com" },
+        { id: 6, name: "User 6", email: "user6@example.com" },
+    ];
+
+    const filteredUsers = users.filter(user => user.name.toLowerCase().includes(query.toLowerCase()));
+
+    return (
+        <div className="w-80 bg-black h-96 rounded-xl overflow-hidden border-2 border-opacity-50 border-blue-900 border-t-yellow-600 border-r-yellow-600">
+            <div className="px-5 pt-5 font-semibold text-lg ">Users List</div>
+            <div className="bg-white/5 h-1/5 px-5 pb-5 flex items-center justify-between">
+                <div className='relative mr-2 '>
+                    <form onSubmit={e => e.preventDefault()}>
+                        <input value={query} onChange={(ev) => { setQuery(ev.target.value); }} type="text" placeholder='Search Users..' className='w-full py-1 px-4 bg-transparent border rounded-lg text-gray-50 font-medium text-sm outline-none' />
+                    </form>
+                    <Icon icon="iconoir:search" className="h-7 w-7 px-1.5 cursor-pointer absolute right-0 top-0 text-white opacity-75" />
+                </div>
+                <button 
+                    className={`flex items-center justify-center text-gray-50 bg-yellow-600 rounded-lg px-2 py-0.5 font-semibold transition-all active:scale-105 cursor-pointer ${!selectedUser ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-700'}`} 
+                    disabled={!selectedUser} 
+                > 
+                    <Icon icon="material-symbols:add" width="18" height="18" className="mr-1" />
+                    Add
+                </button>
+            </div>
+            <div className="overflow-auto h-4/5 bg-white/5 p-5">
+                {filteredUsers.map(user => (
+                    <div 
+                        key={user.id} 
+                        onClick={() => setSelectedUser(user.id)} 
+                        className={`${selectedUser === user.id ? 'bg-white/10' : ''} flex items-center py-2 px-1 hover:bg-white/10 rounded-lg transition-all duration-100 cursor-pointer active:scale-105 `}
+                    >
+                        <img className="border rounded-full w-10 h-10 mr-2" src="" alt="PH" />
+                        <div>
+                            <div className="font-medium">{user.name}</div>
+                            <div className="text-sm opacity-50">{user.email}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
